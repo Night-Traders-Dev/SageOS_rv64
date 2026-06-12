2d337531c43db93879dfa89b518e5a17066db614
Night-Traders-Dev <quegmeister@gmail.com>
2026-06-12 11:37:05 -0400

    feat: link VM core to SG2002 platform

--- sg2002/kernel/vm/platform_vm.c
+++ sg2002/kernel/vm/platform_vm.c
@@ -1,15 +1,19 @@
 #include "platform_vm.h"
 #include "../uart/uart.h"
 #include "../timer/timer.h"
+#include "../../../../sageos_build/sage_lang/core/src/vm/vm.h"
 
 static platform_vm_interface_t* vm_interface;
 
 void sg2002_vm_init(platform_vm_interface_t* interface) {
     vm_interface = interface;
+    // Initialize VM internal state
+    vm_init();
 }
 
 void sg2002_vm_run(void* bytecode) {
-    // 1. Initialize VM state
-    // 2. Set callback interfaces
+    // 1. Load bytecode
+    // 2. Set callback interfaces (via VM-specific mechanism, stubbed here)
     // 3. Start execution loop
+    vm_run(bytecode);
 }
